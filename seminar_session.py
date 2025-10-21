da5eeb5c9d1575a9dfdbe14ecf2e9c5f2e50fcd6
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2025-10-21 10:37:58 -0700

    Update seminar_session.py

--- seminar_session.py
+++ seminar_session.py
@@ -102,9 +102,11 @@ def seminar_session_main(db_connector):
                 st.info(f"Debugging: Looking for slides in this sheet: {enrollment_link}")
                 try:
                     enrollment_ws = db_connector.get_worksheet(enrollment_link, "Seminar_GuestLecture_List")
+                    st.info(f"Debugging: Looking for slides in this sheet: {enrollment_ws}")
                     if enrollment_ws:
                         enrollment_df = db_connector.get_dataframe(enrollment_ws)
                         presenter_row = enrollment_df[enrollment_df['Presentor_FullName'] == live_presenter]
+                        st.info(f"Debugging: Looking for slides in this sheet: {presenter_row}")
                         if not presenter_row.empty:
                             slides_link_from_sheet = presenter_row.iloc[0].get('PresentationLink', '')
                 except Exception:
